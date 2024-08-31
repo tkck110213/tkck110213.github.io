3dd73c7210f2584f536c04cbcf63788ee5f5eb1f
tkck110213 <penetrate.wonder@gmail.com>
2024-09-01 08:11:10 +0900

    change: timeline design

--- components/biography.tsx
+++ components/biography.tsx
@@ -8,20 +8,35 @@ const Biography = () => {
     ];
 
     return (
-        <ol className="relative border-s border-gray-200 dark:border-gray-700"> 
-        {educations.map(({id, date, jp, desc}) => (                 
-            <li key={id} className="ms-6">
-                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                    </svg>
-                </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white"> {jp} </h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"> {date} </time>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400"> {desc} </p>
-            </li>
+        <div>
+        {educations.map(({id, date, jp, desc}) => ( 
+            <div key={id}>
+                <div className="ps-2 my-2 first:mt-0">
+                    <h3 className="text-xs font-medium uppercase text-gray-500 dark:text-neutral-400">
+                    {date}
+                    </h3>
+                </div>
+
+                <div className="flex gap-x-3">
+                    <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+                    <div className="relative z-10 size-7 flex justify-center items-center">
+                        <div className="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
+                    </div>
+                    </div>
+
+                    <div className="grow pt-0.5 pb-8">
+                    <h3 className="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white">
+                        {jp}
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+                        {desc}
+                    </p>
+                    </div>
+                </div>
+            </div>
         ))}
-        </ol>
+        </div>
+        
     )
 }
 
