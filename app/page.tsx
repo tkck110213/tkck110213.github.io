6c5967934f85d90383c96e35b9085b35701dd20e
tkck110213 <penetrate.wonder@gmail.com>
2024-09-01 09:02:04 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "../components/header"
 import Biography from "../components/biography"
+import Recent from "../components/recent"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Recent />
       <Biography />
     </div>
   );
--- components/biography.tsx
+++ components/biography.tsx
@@ -8,7 +8,7 @@ const Biography = () => {
 
     return (
         <div>
-        <h1 className="text-4xl font-semibold flex justify-center items-center mb-14">略歴</h1>
+        <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">略歴</h2>
         {educations.map(({id, date, jp, desc}) => ( 
             <div key={id}>
                 <div className="ps-2 my-2 first:mt-0">
