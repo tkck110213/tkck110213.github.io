99ab7c8ab366df98622c54bc91e8ab1957441fc0
tkck110213 <penetrate.wonder@gmail.com>
2024-09-01 17:38:55 +0900

    fix

--- components/recent.tsx
+++ components/recent.tsx
@@ -1,25 +1,31 @@
 const Recent = () => {
     const news = [
-        { id: 1, desc: "2024/08/31，サイトを開設しました．", title: "" },
-        { id: 2, desc: "2024/10/12-14，日本認知科学会第41回大会にて発表いたします．", title: "亀谷長太，宮本友樹，内海彰: 活性拡散ネットワークとずれの解消理論に基づく詩の鑑賞過程の計算モデル，O-6-4" },
+        { id: 1, date: "2024/08/31", desc: "サイトを開設しました．", title: "" },
+        { id: 2, date: "2024/10/12-14", desc: "日本認知科学会第41回大会にて発表いたします．", title: "亀谷長太，宮本友樹，内海彰: 活性拡散ネットワークとずれの解消理論に基づく詩の鑑賞過程の計算モデル，O-6-4" },
     ];
 
     return (
         <div>
-            <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">近況</h2>
+            <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">NEW</h2>
             <div className="mt-15 mb-16 flex justify-center">
-                <ul className="max-w-xl space-y-1 text-gray-800 list-disc list-inside">
-                    {news.map(({ id, desc, title }) => (
-                        <li key={id} className="list-disc">
-                            {desc}
-                            {title && (
-                                <ul className="list-inside list-circle ml-4">
-                                    <li>{title}</li>
-                                </ul>
-                            )}
-                        </li>
-                    ))}
-                </ul>
+                {news.map(({ id, date, desc, title }) => (
+                    <div key={id}>
+                        <h3 className="text-xl font-medium uppercase text-gray-500">
+                            {date}
+                        </h3>
+                        <ul className="max-w-4xl space-y-1 text-gray-800 list-disc list-inside">
+                            <li  className="list-disc">
+                                {desc}
+                                {title && (
+                                    <ul className="list-inside list-circle ml-4">
+                                        <li>{title}</li>
+                                    </ul>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                ))}
+                
             </div>
         </div>
     );
