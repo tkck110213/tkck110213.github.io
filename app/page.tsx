867825645d75d4e2c4800f5f77f515fe62dd5922
tkck110213 <penetrate.wonder@gmail.com>
2024-09-02 00:18:51 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Header from "../components/header"
 import Biography from "../components/biography"
 import Recent from "../components/recent"
+import About from "../components/about"
 
 export default function Home() {
   return (
     <div>
+      <About />
       <Header />
       <Recent />
       <Biography />
