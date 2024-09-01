eaefc7b344f5770b7c1bd17fb3e15100bdfdfe23
tkck110213 <penetrate.wonder@gmail.com>
2024-09-01 23:10:47 +0900

    fix

--- components/recent.tsx
+++ components/recent.tsx
@@ -5,28 +5,30 @@ const Recent = () => {
     ];
 
     return (
-        <div>
-            <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">NEWS</h2>
-            <div className="mt-15 mb-16 flex flex-col items-center space-y-8">
-                {news.map(({ id, date, desc, title }) => (
-                    <div key={id} className="max-w-xl space-y-1">
-                        <div className="flex justify-start ps-2 my-2 first:mt-0">
-                            <h3 className="text-lg font-medium uppercase text-gray-500">
-                                {date}
-                            </h3>
+        <div className="flex justify-center">
+            <div className="w-full max-w-xl">
+                <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">NEWS</h2>
+                <div className="mt-15 mb-16 space-y-8">
+                    {news.map(({ id, date, desc, title }) => (
+                        <div key={id} className="space-y-1">
+                            <div className="ps-2 my-2 first:mt-0">
+                                <h3 className="text-lg font-medium uppercase text-gray-500">
+                                    {date}
+                                </h3>
+                            </div>
+                            <ul className="space-y-1 text-gray-800 list-disc list-inside">
+                                <li className="list-disc">
+                                    {desc}
+                                    {title && (
+                                        <ul className="list-inside list-circle ml-4">
+                                            <li>{title}</li>
+                                        </ul>
+                                    )}
+                                </li>
+                            </ul>
                         </div>
-                        <ul className="space-y-1 text-gray-800 list-disc list-inside">
-                            <li className="list-disc">
-                                {desc}
-                                {title && (
-                                    <ul className="list-inside list-circle ml-4">
-                                        <li>{title}</li>
-                                    </ul>
-                                )}
-                            </li>
-                        </ul>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
