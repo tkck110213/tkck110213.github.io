a7dc10d8cd0b0252fe4864bfd0e13cff8f046565
tkck110213 <penetrate.wonder@gmail.com>
2024-09-01 16:24:37 +0900

    fix

--- components/biography.tsx
+++ components/biography.tsx
@@ -7,35 +7,33 @@ const Biography = () => {
     ];
 
     return (
-        <div>
-        <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">略歴</h2>
-        {educations.map(({id, date, jp, desc}) => ( 
-            <div key={id}>
-                <div className="ps-2 my-2 first:mt-0">
-                    <h3 className="text-xs font-medium uppercase text-gray-500">
-                    {date}
-                    </h3>
-                </div>
-
-                <div className="flex gap-x-3">
-                    <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200">
-                        <div className="size-2 rounded-full bg-gray-400"></div>
+        <div className="flex flex-col items-center">
+            <h2 className="text-4xl font-semibold mb-14">略歴</h2>
+            {educations.map(({ id, date, jp, desc }) => (
+                <div key={id} className="w-full max-w-xl">
+                    <div className="ps-2 my-2 first:mt-0">
+                        <h3 className="text-xs font-medium uppercase text-gray-500">
+                            {date}
+                        </h3>
                     </div>
-
-                    <div className="grow pt-0.5 pb-8">
-                    <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
-                        {jp}
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-600">
-                        {desc}
-                    </p>
+    
+                    <div className="flex gap-x-3 justify-center">
+                        <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200">
+                            <div className="size-2 rounded-full bg-gray-400"></div>
+                        </div>
+    
+                        <div className="grow pt-0.5 pb-8">
+                            <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
+                                {jp}
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-600">
+                                {desc}
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ))}
+            ))}
         </div>
-        
     )
-}
 
 export default Biography
