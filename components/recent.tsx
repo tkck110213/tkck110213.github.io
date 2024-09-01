749a08af85dc7bbed22f1537ba8c002c2595e3c3
tkck110213 <penetrate.wonder@gmail.com>
2024-09-01 17:52:43 +0900

    fix

--- components/recent.tsx
+++ components/recent.tsx
@@ -1,15 +1,15 @@
 const Recent = () => {
     const news = [
         { id: 1, date: "2024/08/31", desc: "サイトを開設しました．", title: "" },
-        { id: 2, date: "2024/10/12-14", desc: "日本認知科学会第41回大会にて発表いたします．", title: "亀谷長太，宮本友樹，内海彰: 活性拡散ネットワークとずれの解消理論に基づく\n詩の鑑賞過程の計算モデル，O-6-4" },
+        { id: 2, date: "2024/10/12-14", desc: "日本認知科学会第41回大会にて発表いたします．", title: "亀谷長太，宮本友樹，内海彰: 活性拡散ネットワークとずれの解消理論に基づく詩の鑑賞過程の計算モデル，O-6-4" },
     ];
 
     return (
         <div>
-            <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">NEW</h2>
+            <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">NEWS</h2>
             <div className="mt-15 mb-16 flex flex-col items-center space-y-8">
                 {news.map(({ id, date, desc, title }) => (
-                    <div key={id} className="w-full max-w-4xl">
+                    <div key={id} className="max-w-xl space-y-1">
                         <h3 className="text-xl font-medium uppercase text-gray-500">
                             {date}
                         </h3>
