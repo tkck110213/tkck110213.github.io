0e7178a3f730e83d9ed5ca0d24e0edd5a0cfaf11
tkck110213 <penetrate.wonder@gmail.com>
2024-09-02 01:02:22 +0900

    fix

--- components/about.tsx
+++ components/about.tsx
@@ -6,17 +6,13 @@ import Image from "next/image";
 const About = () => {
     return (
         <div id="about" className="flex flex-col items-center">
-            <div className="flex items-center space-x-4">
-                <Image
-                    className="w-16 h-16 rounded-full object-cover"
-                    src="/profile.jpg" 
-                    alt="Profile"
-                />
-                <div>
-                    <h2 className="text-xl font-bold text-gray-800">Chota Kameya</h2>
-                    <p className="text-gray-500">2st year master student at The University of Electro-Communications</p>
-                </div>
-            </div>
+            <Image
+                className="w-16 h-16 rounded-full object-cover"
+                src="/profile.jpg" 
+                alt="Profile"
+            />
+            <h2 className="text-xl font-bold text-gray-800">Chota Kameya</h2>
+            <p className="text-gray-500">2st year master student at The University of Electro-Communications</p>
             <p className="mt-4 text-gray-700">
                 認知科学に関心があります．修士課程では，詩の鑑賞の認知過程の解明に向けて計算モデルを構築しています．
             </p>
