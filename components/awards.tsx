d6e25d48339a662e94130014f24d3485c49b83ff
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-04 23:41:08 +0900

    Increase max width of awards container

--- components/awards.tsx
+++ components/awards.tsx
@@ -6,7 +6,7 @@ const Awards = () => {
 
   return (
     <div id="recent" className="flex justify-center">
-      <div className="w-full max-w-xl">
+      <div className="w-full max-w-4xl">
         <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">
           Awards
         </h2>
