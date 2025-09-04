462da894c34a5860e97d71ee170e3ebda27ef2fd
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-04 15:28:14 +0900

    Refactor Awards component to display awards

---
+++ components/awards.tsx
@@ -0,0 +1,27 @@
+const Awards = () => {
+  const awards = [
+    { id: 2, bib: "電気通信大学 学生表彰 高玉研究室，2023年3月"},
+    { id: 1, bib: "沖縄工業高等専門学校専攻科 創造システム工学専攻 優秀賞，2022年3月"},
+  ];
+
+  return (
+    <div id="recent" className="flex justify-center">
+      <div className="w-full max-w-xl">
+        <h2 className="text-4xl font-semibold flex justify-center items-center mb-14">
+          Publications
+        </h2>
+        <div className="mt-15 mb-16 space-y-8">
+          <ul className="space-y-4">
+            {awards.map(({ id, bib }) => (
+              <li key={id}>
+                {bib}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Archivements;
