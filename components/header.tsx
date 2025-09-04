5b8da080c330bca7b68cb0c171191cbaa35cef65
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-04 23:45:38 +0900

    Update header component to be sticky with shadow

--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,18 @@
-
-
 const Header = () => {
     return(
-        <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl">Chota Kameya</span>
-            </a>
-            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            {/* <a href="#about" className="mr-5 hover:text-gray-900">About</a> */}
-            <a href="#biography" className="mr-5 hover:text-gray-900">Educations</a>
-            <a href="#publications" className="mr-5 hover:text-gray-900">Publications</a>
-            <a href="#awards" className="mr-5 hover:text-gray-900">Awards</a>
-            <a href="#works" className="mr-5 hover:text-gray-900">Works</a>
-            </nav>
-        </div>
+        <header className="sticky top-0 z-50 bg-white text-gray-600 body-font shadow-md">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <span className="ml-3 text-xl">Chota Kameya</span>
+                </a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                    {/* <a href="#about" className="mr-5 hover:text-gray-900">About</a> */}
+                    <a href="#biography" className="mr-5 hover:text-gray-900">Educations</a>
+                    <a href="#publications" className="mr-5 hover:text-gray-900">Publications</a>
+                    <a href="#awards" className="mr-5 hover:text-gray-900">Awards</a>
+                    <a href="#works" className="mr-5 hover:text-gray-900">Works</a>
+                </nav>
+            </div>
         </header>
     )
 }
