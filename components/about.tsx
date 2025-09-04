68db898932e10fa487018e7bf5a5ba2ddb3f271c
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-05 02:47:55 +0900

    Refactor About component structure and content
    Updated the About component layout and text.

--- components/about.tsx
+++ components/about.tsx
@@ -5,15 +5,18 @@ import Image from "next/image";
 const About = () => {
     return (
         <div id="about" className="flex flex-col items-center">
-            <Image
-                className="w-16 h-16 rounded-full object-cover"
-                src="/profile.jpg"
-                alt="Profile"
-            />
-            <h2 className="text-xl font-bold text-gray-800">Chota Kameya</h2>
-            <p className="text-gray-500 text-center">First-year Ph.D. student <br /> The University of Electro-Communications</p>
+            <div className="flex items-center gap-4">
+                <Image
+                    className="w-16 h-16 rounded-full object-cover"
+                    src="/profile.jpg"
+                    alt="Profile"
+                />
+                <h2 className="text-xl font-bold text-gray-800">Chota Kameya</h2>
+                <p className="text-gray-500 text-center">First-year Ph.D. student</p>
+            </div>
+            <p className="mt-4 text-gray-500 text-center">The University of Electro-Communications</p>
             <p className="mt-4 text-gray-700">
-                芸術の美的鑑賞プロセスに関心があります．主に認知科学の領域で活動しています．
+                芸術の美的鑑賞プロセスと認知モデルに関心があります．<br /> 主に認知科学の分野で活動しています．
             </p>
             <div className="mt-4 flex space-x-4 text-gray-600">
                 <a href="https://github.com/tkck110213" aria-label="GitHub">
