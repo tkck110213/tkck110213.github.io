015cec062d809a1f7813a5db765011081a83473d
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-05 01:59:45 +0900

    Pass activeContent and setActiveContent to Header

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,18 @@ import Awards from "@/components/awards";
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Header activeContent={activeContent} setActiveContent={setActiveContent} />
       <div className="container mx-auto flex flex-col md:flex-row mt-8">
-        <aside className="w-full md:w-1/5 p-4">
+        <aside className="w-full md:w-1/4 p-4">
           <About />
         </aside>
-        <main className="w-full md:w-4/5 p-4">
-          <Recent />
-          <Biography />
-          <Archivements />
-          <Awards />
+        
+        <main className="w-full md:w-3/4 p-4">
+          {activeContent === 'news' && <Recent />}
+          {activeContent === 'educations' && <Biography />}
+          {activeContent === 'publications' && <Archivements />}
+          {activeContent === 'awards' && <Awards />}
+          {/* activeContent === 'works' && <Works /> */}
         </main>
       </div>
     </div>
