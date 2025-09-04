32c213be2d9fafc74beb10b5abbfdef680215b4b
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-05 01:22:47 +0900

    Change header background color to gray-50

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return(
-        <header className="sticky top-0 z-50 bg-white text-gray-600 body-font shadow-md">
+        <header className="sticky top-0 z-50 bg-gray-50 text-gray-600 body-font shadow-md">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <span className="ml-3 text-xl">Chota Kameya</span>
