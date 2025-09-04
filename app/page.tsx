496559b282ce6780d5927769a68e702aad7b0b65
tkck110213 <83829524+tkck110213@users.noreply.github.com>
2025-09-05 04:01:25 +0900

    Adjust sidebar and main content widths

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
       <Header activeContent={activeContent} setActiveContent={setActiveContent} />
       
       <div className="container mx-auto flex flex-col md:flex-row mt-8">
-        <aside className="w-full md:w-0.22 p-4">
+        <aside className="w-full md:w-0.25 p-4">
           <About />
         </aside>
         
-        <main className="w-full md:w-0.78 p-4">
+        <main className="w-full md:w-0.75 p-4">
           {activeContent === 'news' && <Recent />}
           {activeContent === 'educations' && <Biography />}
           {activeContent === 'publications' && <Archivements />}
